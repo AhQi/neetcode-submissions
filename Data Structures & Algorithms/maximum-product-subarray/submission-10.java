85346b1a1c71fd1b49478c6513ae022c2c09f0e7
Bill Zhang <ah941206@gmail.com>
2026-06-23 21:41:48 -0700

    Add: maximum-product-subarray - submission-10

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-10.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int curMin = 1, curMax = 1;
+        int res = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = curMin*nums[i];
+            curMin = Math.min(nums[i], Math.min(tmp, nums[i] * curMax));
+            curMax = Math.max(nums[i], Math.max(nums[i] * curMax, tmp));
+
+            res = Math.max(res, curMax);
+        }
+
+        return res;
+    }
+}
