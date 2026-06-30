87b6a60a291f39054fece77ecdcf9ab48475e67d
Bill Zhang <ah941206@gmail.com>
2026-06-29 21:39:06 -0700

    Add: partition-equal-subset-sum - submission-7

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-7.java
@@ -0,0 +1,42 @@
+class Solution {
+    int[][] memo;
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        
+        for (int num : nums) {
+            sum += num;
+        }
+        memo = new int[nums.length+1][sum/2+1];
+        for (int[] m : memo) {
+            Arrays.fill(m, -1);
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+        return helper(nums, -1, sum/2);
+    }
+
+    private boolean helper(int[] nums, int index, int sum) {
+        if (sum == 0) {
+            return true;
+        }
+
+        if (memo[index+1][sum] != -1) {
+            return memo[index+1][sum] == 1 ? true : false;
+        }
+        for (int i = index+1; i < nums.length; i++) {
+            if (sum - nums[i] >= 0 && helper(nums, i, sum - nums[i])) {
+                memo[index+1][sum] = 1;
+                return true;
+            }
+            if (helper(nums, i, sum)) {
+                memo[index+1][sum] = 1;
+                return true;
+            }
+        }
+
+        memo[index+1][sum] = 0;
+        return false;
+    }
+}
