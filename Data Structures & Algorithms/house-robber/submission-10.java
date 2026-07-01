fbc8a62379e5a7ac72fc6c5b423caedfa9e5a189
Bill Zhang <ah941206@gmail.com>
2026-06-30 21:11:40 -0700

    Add: house-robber - submission-10

---
+++ Data Structures & Algorithms/house-robber/submission-10.java
@@ -0,0 +1,37 @@
+class Solution {
+    Map<Integer, Integer> map;
+    public int rob(int[] nums) {
+        map = new HashMap<>();
+        int n = nums.length;
+        int[] dp = new int[n];
+        if (n == 1) {
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        }
+        return Math.max(dp[n-1], dp[n-2]);
+    }
+
+    private int helper(int[] nums, int index) {
+        if (index >= nums.length) {
+            return 0;
+        }
+        if (nums.length - 1 == index) {
+            return nums[index];
+        }
+        if (map.get(index) != null) {
+            return map.get(index);
+        }
+        map.put(index, Math.max(nums[index] + helper(nums, index+2), helper(nums, index+1)));
+
+        return map.get(index);
+    }
+
+
+
+
+    
+}
